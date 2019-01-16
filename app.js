9ca5109a6c6cab32594051b808db86c350e4bd98
Younes <younesberrada1@gmail.com>
2019-01-16 17:12:03 +0100

    added app.js

---
+++ app.js
@@ -0,0 +1,28 @@
+'use strict';
+var fs = require('fs');
+var express=  require('express');
+var bodyParser = require('body-parser');
+var app= express();
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+
+app.get('/', function (req, res) {
+  res.render('index', {val: null,val2:null, error: null});
+})
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/', function (req, res) {
+
+  var contents = fs.readFileSync(req.body.chat, 'utf8');
+        if (err) {
+          console.log(err);
+        } else {
+      }
+      });
+
+})
+app.listen(3000,()=>{
+  console.log("output to port 3000");
+})
